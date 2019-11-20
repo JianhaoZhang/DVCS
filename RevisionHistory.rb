4d77336da437ab20ffaa8e0f0e74c8b4f69d87af
Shuang Zhai <zhaishuang1@gmail.com>
2019-11-20 15:10:08 -0500

    update

--- RevisionHistory.rb
+++ RevisionHistory.rb
@@ -1,1 +1,39 @@
+require_relative "RevisionNode.rb"
+class RevisionHistory
+    def initialize()
+        @head = nil
+        @temp = nil
+        @hashCount = {}
+    end
 
+    def addFile(path)
+        if @temp.nil?
+            @temp = RevisionNode.new()
+        end
+
+        # Get hash of a file
+        hash = "advef"
+        h = @temp.addFile(path, hash)
+
+        if @hashCount[hash].nil?
+            @hashCount[hash] = 1
+        # use file system module to copy file
+        else
+            @hashCount[hash] += 1
+        end
+    end
+
+    def removeFile(path)
+        
+    end
+
+    def commit()
+        if @temp.nil?
+            puts "Nothing to commit"
+            return 1
+        end
+    end
+end
+
+rh = RevisionHistory.new()
+rh.addFile("a.txt")
---
+++ RevisionNode.rb
@@ -0,0 +1,38 @@
+module RevisionState
+    INITIALIZED = 1
+    MODIFIED = 2
+    COMMITED = 3
+end
+
+class RevisionNode
+    def initialize()
+        @commitID = 0
+        @commitMsg = ""
+        @fileHash = {}
+        @state = RevisionState::INITIALIZED
+        @prev = nil
+        @next = nil
+    end
+
+    def setCommitId(id)
+        @commitID = id
+    end
+
+    def setCommitMsg(msg)
+        @commitMsg = msg
+    end
+
+    def deleteFile(path)
+        # check if the hash(path) appears in parent nodes
+        # if not, delete from metadata
+        # maybe need a counter for each hash file
+    end
+
+    def addFile(path, hash)
+        # get the file hash
+        @fileHash[path] = hash
+    end
+
+    def removeFile(path)
+    end
+end
