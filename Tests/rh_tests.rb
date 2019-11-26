f85bf412082341a7f75bd4f46a37ebfcb506bb84
Brendon Pon <brendonpon@brendons-mbp.home>
2019-11-26 15:31:19 -0500

    rh tests commit updated

--- Tests/rh_tests.rb
+++ Tests/rh_tests.rb
@@ -1,6 +1,7 @@
+require 'digest'
 require "minitest/autorun"
 require "open3"
-require_relative "../RevisionHistor.rb"
+require_relative "../RevisionHistory.rb"
 
 class Test_p1 < Minitest::Test
   def setup
@@ -10,11 +11,65 @@ class Test_p1 < Minitest::Test
     system("mkdir Project \n cd Project \n touch new.txt\n")
     
   end
-  def test_init
-    rh = RevisionHistory.new(Dir.pwd, true)
-    puts rh.log()
-    assert true
+  def test_init_new
+
+    Dir.chdir("Project") do
+      rh = RevisionHistory.new(Dir.pwd, true)
+      assert_equal "Revision history is empty.", rh.log()
+      
+    end
   end
+  
+  def test_commit
+    
+    Dir.chdir("Project") do
+      rh = RevisionHistory.new(Dir.pwd, true)
+      assert_equal "Revision history is empty.", rh.log()
+      rh.add("new.txt")
+      File.open("new.txt", "w") {|f| f.write("Something here")}
+      rh.setCommitMsg("Commit 1\nSecond Line")
+      rh.commit()
+      puts rh.log()
+      #puts "after log"
+      
+      File.open("new.txt", "w"){|f| f.write("1 2 3 4 5\n 1234 \n 123 \n12 \n1")}
+      #puts File.open("new.txt", "r") {|f| f.read()}
+      rh.add("new.txt")
+      
+      #puts rh.heads()
+      #puts rh.status()
+      rh.setCommitMsg("Commit 2\nSecond Line")
+      rh.commit()
+      log = rh.log()
+      puts "This is log 1"
+      puts log
+      
+      rh.rh2Text()
+      
+      rh2 = RevisionHistory.new(Dir.pwd, false)
+      log2 = rh2.log()
+      puts "This is log 2"
+      puts log2
+      puts "End log 2"
+      assert_equal log, log2
+      assert true
+    end
+  end
+  
+  def test_init_file
+    
+    Dir.chdir("Project") do
+      rh = RevisionHistory.new(Dir.pwd, true)
+      assert_equal "Revision history is empty.", rh.log()
+      rh.add("new.txt")
+      rh.commit()
+      puts rh.log()
+      File.open("new.txt", "w"){|f| f.write("1 2 3 4 5")}
+    end
+  end
+  
+ 
+  
   def teardown
     if Dir.exist?("Project")
       system("rm -r Project")
@@ -24,3 +79,4 @@ class Test_p1 < Minitest::Test
     end
   end
 end
+
